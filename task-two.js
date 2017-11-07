c9bc861d68b879eef2b488a352d111705f66d029
Fea-Sin <1210992614@qq.com>
2017-11-07 18:05:52 +0800

    change README

--- task-two.js
+++ task-two.js
@@ -3,7 +3,7 @@ var Task = require('shell-task')
 new Task('sleep 1000')
 		.then('git add .')
 		.then('sleep 1000')
-		.then('git commit -m "first commit"')
+		.then('git commit -m "change README"')
 		.then('git push -u origin master')
 		.run(function(err, next) {
 			if (err) {
